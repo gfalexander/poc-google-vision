fc0ecef20f90d0cb26cba067b0bd9c48b6fc2edc
gfalexander <alexander.ferreira@sptech.school>
2023-02-08 13:41:40 -0300

    Adding more methods

--- src/main/java/GvisionApplication.java
+++ src/main/java/GvisionApplication.java
@@ -46,7 +46,7 @@ public class GvisionApplication {
         }
     }
 
-    public static void detectTextInImage(String imgPath) throws IOException {
+    public static List<String> detectTextInImage(String imgPath) throws IOException {
         List<AnnotateImageRequest> requests = new ArrayList<>();
 
         ByteString imgBytes = ByteString.readFrom(new FileInputStream(imgPath));
@@ -60,28 +60,140 @@ public class GvisionApplication {
         try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
             BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
             List<AnnotateImageResponse> responses = response.getResponsesList();
-            List<String> teste = new ArrayList<>();
+            List<String> content = new ArrayList<>();;
             for (AnnotateImageResponse res : responses) {
                 if (res.hasError()) {
                     System.out.format("Error: %s%n", res.getError().getMessage());
-                    return;
                 }
+                List<EntityAnnotation> annotations = res.getTextAnnotationsList();
 
-                for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
-
-                    if (annotation.getDescription().contains("FASICO")) {
-                        teste.add(annotation.getDescription());
+                for (int i = 0; i < annotations.size(); i++) {
+                    if (i > 0) {
+                        content.add(annotations.get(i).getDescription());
                     }
-//                    System.out.format("Text: %s%n", annotation.getDescription());
-//                    System.out.format("Position : %s%n", annotation.getBoundingPoly());
                 }
+//                for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+//
+//                    if (annotation.getDescription().contains("FASICO")) {
+//                        teste.add(annotation.getDescription());
+//                    }
+////                    System.out.format("Text: %s%n", annotation.getDescription());
+////                    System.out.format("Position : %s%n", annotation.getBoundingPoly());
+//                }
             }
-            System.out.println(teste);
+            return content;
         }
     }
 
+    public static void filterMonthInfo(List<String> contents) {
+        List<String> monthInfo = new ArrayList<>();
+        boolean flagMonth = false;
+        int indexMonth = 12;
+
+        for (String content : contents) {
+            if (flagMonth && indexMonth > 0) {
+                monthInfo.add(content);
+                indexMonth--;
+            }
+            if (content.equals("Més") || content.equals("Mês") || content.equals("Mes")) {
+                flagMonth = true;
+                monthInfo.add(content);
+            }
+        }
+
+        System.out.println(monthInfo);
+    }
+
+    public static void filterKwh(List<String> contents) {
+        List<String> measuresKwh = new ArrayList<>();
+        boolean flagKwh = false;
+        int indexKwh = 12;
+        int tryIndex = 3;
+
+        for (String content : contents) {
+            if (flagKwh && indexKwh > 0) {
+                if (tryIndex > 0) {
+                    try {
+                        if(content.contains("ооо")) {
+                            List<String> contentToZero = List.of(content.split(""));
+
+                            for (int i = 0; i < contentToZero.size(); i++) {
+                                measuresKwh.add("0");
+                                indexKwh--;
+                            }
+                        } else {
+                            Integer.valueOf(content);
+                            measuresKwh.add(content);
+                            indexKwh--;
+                        }
+                    } catch (Exception e) {
+                        tryIndex--;
+                        if (tryIndex == 0) {
+                            tryIndex = 3;
+                            flagKwh = false;
+                            measuresKwh.remove(0);
+                        }
+                    }
+                }
+            }
+            if (content.equals("kWh")) {
+                if (measuresKwh.isEmpty()) {
+                    measuresKwh.add(content);
+                }
+                flagKwh = true;
+            }
+        }
+            System.out.println(measuresKwh);
+    }
+
+    public static void filterMeasurer(List<String> contents) {
+        List<String> measurers = new ArrayList<>();
+
+        for (String content : contents) {
+            if (content.contains("FASICO")) {
+                measurers.add(content);
+            }
+        }
+
+        System.out.println(measurers);
+    }
+
+    public static void filterTariffs(List<String> contents) {
+        List<String> tariffs = new ArrayList<>();
+        boolean flagTariffs = false;
+        int tryIndex = 2;
+
+        for (String content : contents) {
+            if (flagTariffs) {
+                try {
+                    content = content.replace(",", ".");
+
+                    Double.valueOf(content);
+                    tariffs.add(content);
+                    flagTariffs = false;
+
+                } catch (Exception e) {
+                    tryIndex--;
+                }
+            }
+
+            if (content.equals("Tarifas")) {
+                tariffs.add(content);
+                flagTariffs = true;
+            }
+        }
+        System.out.println(tariffs);
+
+    }
+
     public static void main(String[] args) throws IOException {
-        String filePath = "C:\\Users\\Consultor\\Documents\\Matrix\\poc-gvision\\src\\main\\resources\\img\\com_flash.jpg";
-        detectTextInImage(filePath);
+        String tmpPath = System.getProperty("java.io.tmpdir");
+        String filePath = String.format("%s/com_flash.jpg", tmpPath);
+        List<String> contents = detectTextInImage(filePath);
+
+        filterMonthInfo(contents);
+        filterMeasurer(contents);
+        filterKwh(contents);
+        filterTariffs(contents);
     }
 }
